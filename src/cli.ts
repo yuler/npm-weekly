fa186783888adfe85d9d74f077e25da7ec187491
yuler <is.yuler@gmail.com>
2022-08-29 23:05:40 +0800

    wip: define cli

---
+++ src/cli.ts
@@ -0,0 +1,133 @@
+import type { Argv } from 'yargs'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import c from 'picocolors'
+import { version } from '../package.json'
+import type { CommonOptions } from './types'
+
+function commonOptions(args: Argv<{}>): Argv<CommonOptions> {
+	return args
+		.option('cwd', {
+			alias: 'C',
+			default: '',
+			type: 'string',
+			describe: 'specify the current working directory',
+		})
+		.option('loglevel', {
+			default: 'info',
+			type: 'string',
+			describe: 'log level',
+			choices: LOGLEVELS,
+		})
+		.option('silent', {
+			alias: 's',
+			default: false,
+			type: 'boolean',
+			describe: 'complete silent',
+		})
+		.option('recursive', {
+			alias: 'r',
+			type: 'boolean',
+			describe: 'recursively search for package.json in subdirectories',
+		})
+		.option('force', {
+			alias: 'f',
+			type: 'boolean',
+			describe: 'force fetching from server, bypass cache',
+		})
+		.option('include', {
+			alias: 'n',
+			type: 'string',
+			describe: 'only included dependencies will be checked for updates',
+		})
+		.option('exclude', {
+			alias: 'x',
+			type: 'string',
+			describe: 'exclude dependencies to be checked, will override --include options',
+		})
+		.option('dev', {
+			alias: 'D',
+			type: 'boolean',
+			describe: 'update only for devDependencies',
+			conflicts: ['prod'],
+		})
+		.option('prod', {
+			alias: 'P',
+			type: 'boolean',
+			describe: 'update only for dependencies',
+			conflicts: ['dev'],
+		})
+}
+
+yargs(hideBin(process.argv))
+	.scriptName('taze')
+	.usage('$0 [args]')
+	.command(
+		'usage',
+		'List dependencies versions usage across packages',
+		(args) => {
+			return commonOptions(args)
+				.option('detail', {
+					alias: 'a',
+					type: 'boolean',
+					default: false,
+					describe: 'show more info',
+				})
+				.help()
+				.demandOption('recursive', c.yellow('Please add -r to analysis usages'))
+		},
+		async (args) => usage(await resolveConfig({ ...args, recursive: true }))
+	)
+	.command(
+		'* [mode]',
+		'Keeps your deps fresh',
+		(args) => {
+			return commonOptions(args)
+				.positional('mode', {
+					default: 'default',
+					type: 'string',
+					describe: 'the mode how version range resolves, can be "default", "major", "minor", "latest" or "newest"',
+					choices: ['default', 'major', 'minor', 'patch', 'latest', 'newest'],
+				})
+				.option('write', {
+					alias: 'w',
+					default: false,
+					type: 'boolean',
+					describe: 'write to package.json',
+				})
+				.option('interactive', {
+					alias: 'I',
+					default: false, // TODO: enable by default: !process.env.CI && process.stdout.isTTY,
+					type: 'boolean',
+					describe: 'interactive mode',
+				})
+				.option('install', {
+					alias: 'i',
+					default: false,
+					type: 'boolean',
+					describe: 'install directly after bumpping',
+				})
+				.option('update', {
+					alias: 'u',
+					default: false,
+					type: 'boolean',
+					describe: 'update directly after bumpping',
+				})
+				.option('all', {
+					alias: 'a',
+					default: false,
+					type: 'boolean',
+					describe: 'show all packages up to date info',
+				})
+				.help()
+		},
+		async (args) => {
+			await check(await resolveConfig(args))
+			process.exit()
+		}
+	)
+	.showHelpOnFail(false)
+	.alias('h', 'help')
+	.version('version', version)
+	.alias('v', 'version')
+	.help().argv
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type RangeMode = 'default' | 'major' | 'minor' | 'patch' | 'latest' | 'newest'
+
+export type PackageMode = Omit<RangeMode, 'default'> | 'ignore'
+
+export interface CommonOptions {
+	cwd: string
+	recursive?: boolean
+	include?: string | string[]
+	exclude?: string | string[]
+	prod?: boolean
+	dev?: boolean
+	loglevel: string
+	silent?: boolean
+	force?: boolean
+	packageMode?: { [name: string]: PackageMode }
+}
